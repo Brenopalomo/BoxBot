5e56f4fda97eb99309acfa403fbc54a4ba175cf1
Brenopalomo <76840613+Brenopalomo@users.noreply.github.com>
2021-02-10 18:17:47 -0500

    index.js

--- index.js
+++ index.js
@@ -18,12 +18,12 @@ const arrayBulan = ['Januari', 'Februari', 'Maret', 'April', 'Mei', 'Juni', 'Jul
 const bulan = arrayBulan[moment().format('MM') - 1]
 
 const config = {
-    A187: '📍D4RKBOT📍',
+    A187: 'BOXBOT📦',
     instagram: 'OFF',
-    nomer: 'wa.me/21997123716',
+    nomer: 'wa.me/+5516999916885',
     aktif: '24 JAM',
-    youtube: 'https://youtube.com/channel/UCRaC-6JyCW_iqbW1DbuXVxQ',
-    whatsapp: 'https://chat.whatsapp.com/KTlC0MXi3WJChdQeps5flt',
+    youtube: 'Breno Rodrigo',
+    whatsapp: '+5516999916885',
     tanggal: `TANGGAL: ${moment().format('DD')} ${bulan} ${moment().format('YYYY')}`,
     waktu: time
 }
